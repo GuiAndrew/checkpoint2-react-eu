ea4565b8cd0d499b8f5321591956211217adfcb1
GuiAndrew <guiandrew.pt@gmail.com>
2020-03-26 19:31:27 +0000

    create components

---
+++ fetching-games/src/components/GameList/GameList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Gamelist extends Component {
+    constructor() {
+
+    }
+
+    render() {    
+        return (
+            <div className="">
+                
+            </div>
+        );
+    }
+}
+
+export default Gamelist;
---
+++ fetching-games/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import Game from '../Game/Game';
+
+const Home = () => {
+  return (
+    <>
+      Hello
+      <Game />
+    </>
+  );
+}
+
+export default Home;
